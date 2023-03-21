02152a109014e2d6581740112da9673ca7d6744d
morphius <inneskgibson@gmail.com>
2023-03-21 17:13:36 +0000

    Chnage to footer and database

---
+++ mockData.js
@@ -0,0 +1,188 @@
+const mockRecipesData = {
+  mainCourses: [
+    {
+      id: 1,
+      name: "Spaghetti Bolognese",
+      description: "A classic Italian pasta dish with meat sauce.",
+      imageUrl: "https://example.com/spaghetti-bolognese.jpg"
+    },
+    {
+      id: 2,
+      name: "Chicken Alfredo",
+      description: "Creamy pasta with chicken and parmesan cheese.",
+      imageUrl: "https://example.com/chicken-alfredo.jpg"
+    },
+    {
+      id: 3,
+      name: "Taco Salad",
+      description: "A fresh and flavorful salad with seasoned ground beef, beans, and vegetables.",
+      imageUrl: "https://example.com/taco-salad.jpg"
+    },
+    {
+      id: 4,
+      name: "Vegetable Stir Fry",
+      description: "A healthy mix of vegetables cooked in a savory sauce.",
+      imageUrl: "https://example.com/vegetable-stir-fry.jpg"
+    },
+    {
+      id: 5,
+      name: "Grilled Salmon",
+      description: "A delicious grilled salmon with a tangy sauce.",
+      imageUrl: "https://example.com/grilled-salmon.jpg"
+    },
+    {
+      id: 6,
+      name: "Beef Stroganoff",
+      description: "A creamy and savory dish with beef, mushrooms, and onions.",
+      imageUrl: "https://example.com/beef-stroganoff.jpg"
+    },
+    {
+      id: 7,
+      name: "Lentil Soup",
+      description: "A hearty and healthy lentil soup with vegetables and spices.",
+      imageUrl: "https://example.com/lentil-soup.jpg"
+    },
+    {
+      id: 8,
+      name: "Chicken Curry",
+      description: "A flavorful chicken dish cooked in a rich, spiced sauce.",
+      imageUrl: "https://example.com/chicken-curry.jpg"
+    },
+    {
+      id: 9,
+      name: "Shrimp Scampi",
+      description: "A delicious shrimp dish cooked in a garlic butter sauce.",
+      imageUrl: "https://example.com/shrimp-scampi.jpg"
+    },
+    {
+      id: 10,
+      name: "Vegetarian Lasagna",
+      description: "A tasty and satisfying vegetarian lasagna with layers of pasta, vegetables, and cheese.",
+      imageUrl: "https://example.com/vegetarian-lasagna.jpg"
+    }
+  ],
+  desserts: [
+    {
+      id: 11,
+      name: "Chocolate Cake",
+      description: "A rich, moist chocolate cake with chocolate frosting.",
+      imageUrl: "https://example.com/chocolate-cake.jpg"
+    },
+    {
+      id: 12,
+      name: "Cheesecake",
+      description: "A creamy, delicious cheesecake with a graham cracker crust.",
+      imageUrl: "https://example.com/cheesecake.jpg"
+    },
+    {
+      id: 13,
+      name: "Apple Pie",
+      description: "A classic dessert with a flaky crust and sweet, spiced apple filling.",
+      imageUrl: "https://example.com/apple-pie.jpg"
+    },
+    {
+      id: 14,
+      name: "Lemon Bars",
+      description: "Sweet and tangy lemon bars with a buttery crust.",
+      imageUrl: "https://example.com/lemon-bars.jpg"
+    },
+    {
+  id: 15,
+  name: "Tiramisu",
+  description: "A classic Italian dessert with layers of coffee-soaked ladyfingers and mascarpone cheese.",
+  imageUrl: "https://example.com/tiramisu.jpg"
+},
+{
+  id: 16,
+  name: "Brownies",
+  description: "Rich and fudgy chocolate brownies with a crackly top.",
+  imageUrl: "https://example.com/brownies.jpg"
+},
+{
+  id: 17,
+  name: "Panna Cotta",
+  description: "A silky and creamy Italian dessert topped with fruit or a sweet sauce.",
+  imageUrl: "https://example.com/panna-cotta.jpg"
+},
+{
+  id: 18,
+  name: "Creme Brulee",
+  description: "A rich and creamy custard dessert with a caramelized sugar topping.",
+  imageUrl: "https://example.com/creme-brulee.jpg"
+},
+{
+  id: 19,
+  name: "Ice Cream",
+  description: "A frozen dessert made from cream and sugar, available in various flavors.",
+  imageUrl: "https://example.com/ice-cream.jpg"
+},
+{
+  id: 20,
+  name: "Fruit Tart",
+  description: "A sweet pastry crust filled with custard and topped with fresh fruit.",
+  imageUrl: "https://example.com/fruit-tart.jpg"
+}
+],
+popularDishes: [
+{
+  id: 21,
+  name: "Pizza Margherita",
+  description: "A classic Italian pizza with tomato, mozzarella, and basil.",
+  imageUrl: "https://example.com/pizza-margherita.jpg"
+},
+{
+  id: 22,
+  name: "Sushi",
+  description: "A Japanese dish with vinegared rice, fish, and other ingredients.",
+  imageUrl: "https://example.com/sushi.jpg"
+},
+{
+  id: 23,
+  name: "Burger",
+  description: "A delicious sandwich made with a ground beef patty, cheese, and various toppings.",
+  imageUrl: "https://example.com/burger.jpg"
+},
+{
+  id: 24,
+  name: "Ramen",
+  description: "A Japanese noodle soup with a rich broth, meat, and vegetables.",
+  imageUrl: "https://example.com/ramen.jpg"
+},
+{
+  id: 25,
+  name: "Pad Thai",
+  description: "A popular Thai dish made with rice noodles, vegetables, and a tangy sauce.",
+  imageUrl: "https://example.com/pad-thai.jpg"
+},
+{
+  id: 26,
+  name: "Fish and Chips",
+  description: "A classic British dish with battered, fried fish and crispy chips.",
+  imageUrl: "https://example.com/fish-and-chips.jpg"
+},
+{
+  id: 27,
+  name: "Steak",
+  description: "A juicy, tender cut of beef cooked to perfection.",
+  imageUrl: "https://example.com/steak.jpg"
+},
+{
+  id: 28,
+  name: "Tacos",
+  description: "A Mexican dish with a variety of fillings served in a soft or crunchy tortilla.",
+  imageUrl: "https://example.com/tacos.jpg"
+},
+{
+  id: 29,
+  name: "Paella",
+description: "A Spanish rice dish cooked with a variety of meats, seafood, and vegetables.",
+imageUrl: "https://example.com/paella.jpg"
+},
+{
+id: 30,
+name: "Pasta Carbonara",
+description: "A creamy Italian pasta dish made with eggs, cheese, pancetta, and pepper.",
+imageUrl: "https://example.com/pasta-carbonara.jpg"
+}
+]
+};
